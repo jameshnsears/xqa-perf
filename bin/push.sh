f65c88b0c0d94bddcf59ef6a1df49f007e06cf62
James Sears <james.hn.sears@gmail.com>
2018-03-16 17:15:35 +0000

    make sure push.sh includes everything

--- bin/push.sh
+++ bin/push.sh
@@ -14,13 +14,13 @@ function publish_to_docker_hub() {
 
 publish_to_docker_hub xqa-db
 publish_to_docker_hub xqa-db-amqp
-# publish_to_docker_hub xqa-elk-elasticsearch
-# publish_to_docker_hub xqa-elk-kibana
-# publish_to_docker_hub xqa-elk-logstash
+publish_to_docker_hub xqa-elk-elasticsearch
+publish_to_docker_hub xqa-elk-kibana
+publish_to_docker_hub xqa-elk-logstash
 publish_to_docker_hub xqa-ingest
 publish_to_docker_hub xqa-ingest-balancer
-# publish_to_docker_hub xqa-message-broker
-# publish_to_docker_hub xqa-message-broker-filebeat
+publish_to_docker_hub xqa-message-broker
+publish_to_docker_hub xqa-message-broker-filebeat
 publish_to_docker_hub xqa-query-ui
 publish_to_docker_hub xqa-shard
 
