cdfe2f60131a3aac6e34945f9b05e3906aa857ec
James Sears <james.hn.sears@gmail.com>
2018-03-16 17:52:59 +0000

    allow e2e.sh to run with default params

--- bin/e2e.sh
+++ bin/e2e.sh
@@ -5,6 +5,11 @@ docker rm $(docker ps -a -q) > /dev/null 2>&1
 docker network prune -f > /dev/null 2>&1
 docker volume prune -f > /dev/null 2>&1
 
+if [ -z "$1" ]; then
+    YML="../docker-compose.dev.yml"
+else
+    YML=$1
+fi
 if [[ -z "${POOL_SIZE}" ]]; then
     POOL_SIZE=2
 fi
@@ -12,7 +17,7 @@ if [[ -z "${SHARDS}" ]]; then
     SHARDS=1
 fi
 
-docker-compose -p "dev" -f $1 up -d --scale xqa-shard=$SHARDS
+docker-compose -p "dev" -f $YML up -d --scale xqa-shard=$SHARDS
 
 docker run -d --net="dev_xqa" --name="dev_xqa-ingest_1" -v $HOME/GIT_REPOS/xqa-test-data:/xml xqa-ingest:latest -message_broker_host xqa-message-broker -path /xml
 
