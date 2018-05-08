609211cc0650600281b31a17dcbc51d946fdbaab
James Sears <james.hn.sears@gmail.com>
2018-05-08 11:39:47 +0100

    wip

--- bin/e2e.sh
+++ bin/e2e.sh
@@ -9,15 +9,21 @@ if [ -z "$1" ]; then
 else
     YML=$1
 fi
+
 if [[ -z "${POOL_SIZE}" ]]; then
     POOL_SIZE=2
 fi
+
 if [[ -z "${SHARDS}" ]]; then
     SHARDS=2
 fi
 
+if [[ -z "${XQA_TEST_DATA}" ]]; then
+    XQA_TEST_DATA=$HOME/GIT_REPOS/xqa-test-data
+fi
+
 docker-compose -p "dev" -f $YML up -d --scale xqa-shard=$SHARDS
 
-docker run -d --net="dev_xqa" --name="dev_xqa-ingest_1" -v $HOME/GIT_REPOS/xqa-test-data:/xml xqa-ingest:latest -message_broker_host xqa-message-broker -path /xml
+docker run -d --net="dev_xqa" --name="dev_xqa-ingest_1" -v $XQA_TEST_DATA:/xml xqa-ingest:latest -message_broker_host xqa-message-broker -path /xml
 
 exit $?
